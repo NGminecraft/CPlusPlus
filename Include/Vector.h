40f4f0a1cf68f6b1cf857b3ecfc5bde07369510b
NGminecraft <99756151+NGminecraft@users.noreply.github.com>
2025-11-17 13:32:25 -0700

    Basic vector class!

--- Include/Vector.h
+++ Include/Vector.h
@@ -2,18 +2,30 @@
 // Created by grahanic001 on 11/17/2025.
 //
 
+#include <sstream>
+
 #ifndef VECTOR_H
 #define VECTOR_H
 
 
 template <typename T, int N>
 struct Vector {
-    T* data[N];
+    T data[N];
     int length = N;
 
     Vector() = default;
     explicit Vector(const T *otherData) {
-        memcpy(data, otherData, sizeof(T) * N);
+        memcpy(&data, otherData, sizeof(T) * N);
+    }
+
+    std::string print() {
+        std::stringstream ss;
+        ss << "<" << data[0];
+        for (int i = 1; i < N; i++) {
+            ss << ", " << data[i];
+        }
+        ss << ">";
+        return ss.str();
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 //
 
 #include <iostream>
+#include "Vector.h"
 
 int main() {
+    int test[3] = {1, 2, 3};
+    Vector<int, 3> Hello(test);
+    std::cout << "Hello: " << Hello.print() << std::endl;
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
