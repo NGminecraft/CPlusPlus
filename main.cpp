d8e1d175e3082d878b1c5e847b6c95115a008a0e
NGminecraft <99756151+NGminecraft@users.noreply.github.com>
2025-11-19 12:02:38 -0700

    Screw Vectors

--- main.cpp
+++ main.cpp
@@ -3,13 +3,8 @@
 //
 
 #include <iostream>
-#include "Vector.h"
 
 int main() {
-    int test[3] = {1, 2, 3};
-    RGB Hello(1, 2, 3);
-    std::cout << "Hello: " << Hello.print() << std::endl;
-
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
